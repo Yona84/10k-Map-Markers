3b931973898504c968293620adc5cc1a55a7c7c8
Yona84 <yonathanronen@gmail.com>
2020-06-04 15:40:45 +0300

    fixed some legacy code

--- client/src/components/CarComponent.jsx
+++ client/src/components/CarComponent.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { css } from 'emotion';
-
-const propTypes = {
-  text: PropTypes.string,
-};
 
 const carImgSrc = 'https://img.icons8.com/color/48/000000/car.png';
 
-const CarComponent = ({ text }) => {
+const CarComponent = () => {
   return (
     <div className={styles.root}>
       <img src={carImgSrc} alt='car' width={30} height={30}/>
-      <span className={styles.text}>{text}</span>
     </div>
   );
 };
